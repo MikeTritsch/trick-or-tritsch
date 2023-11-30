411f126ff9c46532ff7c3c817d15645e9518c8cf
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-11-30 13:51:12 -0600

    Created working header and navigation menu. Will settle on final design after MVP is complete.

--- trick-or-tritsch/src/components/Header.jsx
+++ trick-or-tritsch/src/components/Header.jsx
@@ -1,8 +1,10 @@
 const Header = () => {
   return (
     <>
-    <h1 className="home-name">Mike Tritsch</h1> 
-      <p>Full Stack Developer | All Things Music Business</p>
+      <div className='name-container'>
+        <h1 className="home-name">Mike Tritsch</h1> 
+        <p>Full Stack Developer | All Things Music Business</p>
+      </div>
     </>
   )
 }
--- trick-or-tritsch/src/components/Navigation.jsx
+++ trick-or-tritsch/src/components/Navigation.jsx
@@ -1,6 +1,25 @@
 const Navigation = () => {
   return (
-    <h1>Nav</h1>
+<nav className="test navbar navbar-expand-lg">
+  <div className="container-fluid">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span>Menu</span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav m-auto mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">GitHub</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">LinkedIn</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Projects</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
   );
 };
 
