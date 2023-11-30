f4c9590b7f8f6e9a43a9f790040f143e12236b6f
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-11-29 22:10:04 -0600

    Experimenting with bootstrap. Successfully added navbar without it, so we will see '

--- trick-or-tritsch/src/App.jsx
+++ trick-or-tritsch/src/App.jsx
@@ -1,1 +1,14 @@
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import './App.css'
 
+function App() {
+  return (
+    <div>
+      <Header />
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ trick-or-tritsch/src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <h1 className="home-name">Mike Tritsch</h1>
+  )
+}
+//REVIEW - Does React take care of the semantics?
+
+export default Header;
