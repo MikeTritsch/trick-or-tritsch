daf43dc94e85bb7ebc922968cb72091e8461df48
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-12-02 16:42:17 -0600

    Still working on project cards, back and fourth on styling.

--- trick-or-tritsch/src/pages/Portfolio.jsx
+++ trick-or-tritsch/src/pages/Portfolio.jsx
@@ -1,6 +1,8 @@
 import { Card } from 'react-bootstrap';
 import brew from '../assets/images/breweryexp.png'
 import hot from '../assets/images/house-of-tec.png'
+import weather from '../assets/images/weather.png'
+import rolling from '../assets/images/rolling.png'
 
 const Portfolio = () => {
   return (
@@ -8,14 +10,14 @@ const Portfolio = () => {
       <h1>Portfolio</h1>
       <div className="project-cards">
         <Card className="project-card">
-        <Card.Img className="brew-card-img" variant="top" src={brew} />
+        <Card.Img className="card-img" variant="top" src={brew} />
         <Card.Body>
           <Card.Title>Brewery Experience</Card.Title>
           <Card.Text>
             HTML | CSS | JavaScript | jQuery
           </Card.Text>
           <Card.Text>
-          A brewery tracker with two open-sourced APIs: the Google Maps API and the Open Brewery Database API.
+          A brewery tracker with two open-sourced APIs: Google Maps API and the Open Brewery Database API.
           </Card.Text>
         </Card.Body>
       </Card>
@@ -32,9 +34,47 @@ const Portfolio = () => {
         </Card.Body>
       </Card>
       <Card className="project-card">
-        <Card.Img className="brew-card-img" variant="top" src={brew} />
+        <Card.Img className="card-img" variant="top" src={weather} />
         <Card.Body>
-          <Card.Title>Brewery Experience</Card.Title>
+          <Card.Title>Weather or Not</Card.Title>
+          <Card.Text>
+          JavaScript | CSS | HTML | OpenWeather API
+          </Card.Text>
+          <Card.Text>
+          An informative weather forecasting application that provides up-to-date weather information for the next five days.          
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </div>
+      <div className="project-cards">
+        <Card className="project-card">
+        <Card.Img className="card-img" variant="top" src={rolling} />
+        <Card.Body>
+          <Card.Title>Rolling 25</Card.Title>
+          <Card.Text>
+            Handlebars | Bootstrap | JavaScript | Express | Node | mySQL
+          </Card.Text>
+          <Card.Text>
+          A brewery tracker with two open-sourced APIs: the Google Maps API and the Open Brewery Database API.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="project-card">
+        <Card.Img className="card-img" variant="top" src={hot} />
+        <Card.Body>
+          <Card.Title>CSS Code Snippets</Card.Title>
+          <Card.Text>
+          Node | Express | Bootstrap | Handlebars | mySQL
+          </Card.Text>
+          <Card.Text>
+          A user friendy interactive experience where music business nerds and tech peeps can come together to talk shop and share opinions.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="project-card">
+        <Card.Img className="card-img" variant="top" src={brew} />
+        <Card.Body>
+          <Card.Title>JavaScript Madness</Card.Title>
           <Card.Text>
           A brewery tracker with two open-sourced APIs: the Google Maps API and the Open Brewery Database API.
           </Card.Text>
