11cf932de1fa4e4cd5b55631f01fca979fa4a21f
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-12-01 19:46:29 -0600

    Added some styling and icons to the footer.

--- trick-or-tritsch/src/components/Footer.jsx
+++ trick-or-tritsch/src/components/Footer.jsx
@@ -8,9 +8,15 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <div className='icons'>
-        <BsSpotify />
-        <BsGithub />
-        <BsLinkedin />
+        <a href="https://open.spotify.com/user/mtritsch115?si=7ffabf7fc98d4f4c" target="_blank">
+          <BsSpotify />
+        </a>
+        <a href="https://github.com/MikeTritsch" target="_blank">
+          <BsGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/michael-tritsch/" target="_blank">
+          <BsLinkedin />
+        </a>
       </div>
     </footer>
   )
--- trick-or-tritsch/src/pages/Contact.jsx
+++ trick-or-tritsch/src/pages/Contact.jsx
@@ -1,20 +1,20 @@
 const Contact = () => {
   return (
-    <div>
+    <div className="adjust-height">
       <form className="contact-form">
         <div className="mb-3">
-          <label for="exampleFormControlInput1" className="form-label">Name</label>
+          <label htmlFor="exampleFormControlInput1" className="form-label">Name</label>
           <input type="text" className="form-control" id="exampleFormControlInput1" placeholder=""/>
         </div>
         <div className="mb-3">
-          <label for="exampleFormControlInput1" className="form-label">Email address</label>
+          <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
           <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
         </div>
         <div className="mb-3">
-          <label for="exampleFormControlTextarea1" className="form-label">Message</label>
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">Message</label>
           <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
         </div>
-        <button type="submit" class="btn btn-outline-primary">Submit</button>
+        <button type="submit" className="btn btn-outline-primary">Submit</button>
       </form>
     </div>
   )
