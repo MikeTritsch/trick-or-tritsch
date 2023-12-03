aab6496c1e1032a11068f9e92a9083c4bbe2182e
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-12-02 19:44:21 -0600

    Added download PDF fucntionality to resume tab thanks to geeks for geeks.

--- trick-or-tritsch/src/pages/Resume.jsx
+++ trick-or-tritsch/src/pages/Resume.jsx
@@ -1,6 +1,21 @@
 const Resume = () => {
+  const onButtonClick = () => {
+    const pdfUrl = "mtritsch-resume.pdf";
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "Tritsch-Resume.pdf"; // specify the filename
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
+
   return (
-    <h1>Resume</h1>
+    <div className="resume-div">
+      <h1 className="page-name">Resume</h1>
+      <button className="resume-btn btn btn-outline-primary" onClick={onButtonClick}>
+        Download PDF
+      </button>
+    </div>
   )
 }
 
