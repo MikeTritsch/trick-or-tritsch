825fc9d066c2f4ba3e2710e11ccd7485d3ea1a46
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-12-03 11:25:08 -0600

    Added resume styling and hover effects.

--- trick-or-tritsch/src/pages/Resume.jsx
+++ trick-or-tritsch/src/pages/Resume.jsx
@@ -1,3 +1,37 @@
+import { FaCss3 } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { GrMysql } from "react-icons/gr";
+import { DiMongodb } from "react-icons/di";
+import { SiHandlebarsdotjs } from "react-icons/si";
+import { FaBootstrap } from "react-icons/fa";
+import { FaNode } from "react-icons/fa";
+import { DiJqueryLogo } from "react-icons/di";
+import { SiExpress } from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaHtml5 } from "react-icons/fa";
+import { SiMicrosoftoffice } from "react-icons/si";
+import { FaGoogle } from "react-icons/fa";
+import { PiVinylRecordLight } from "react-icons/pi";
+import { FaWordpress } from "react-icons/fa";
+import { FaComputer } from "react-icons/fa6";
+import { SiAdobeindesign } from "react-icons/si";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const Resume = () => {
   const onButtonClick = () => {
     const pdfUrl = "mtritsch-resume.pdf";
@@ -15,60 +49,97 @@ const Resume = () => {
       <button className="resume-btn btn btn-outline-primary" onClick={onButtonClick}>
         Download PDF
       </button>
-      <div className="tech-stack-list">
-        <h2>Tech Stack</h2>
-        <ul>
-          <li>JavaScript</li>
-          <li>Express.js</li>
-          <li>Node.js</li>
-          <li>jQuery</li>
-          <li>HTML5</li>
-          <li>CSS3</li>
-          <li>Bootstrap</li>
-          <li>Handlebars</li>
-          <li>React.js</li>
-          <li>mySQL</li>
-          <li>noSQL/MongoDB</li>
-        </ul>
-        <div className="pro-tech-list">
-          <h2>Professional Tech</h2>
+      <div className="resume-items-div">
+        <div className="tech-stack-list">
+          <h2>Tech Stack</h2>
           <ul>
-            <li>Microsoft 365</li>
-            <li>Google Suite</li>
-            <li>Logic Pro X</li>
-            <li>WordPress</li>
-            <li>Mac & PC</li>
-            <li>Adobe InDesign</li>
+            <li>
+            <IoLogoJavascript />
+              JavaScript
+            </li>
+            <li>
+            <SiExpress />
+              Express.js
+            </li>
+            <li>
+            <FaNode />
+              Node.js
+            </li>
+            <li>
+            <DiJqueryLogo />
+              jQuery
+            </li>
+            <li>
+            <FaHtml5 />
+              HTML5
+            </li>
+            <li>
+            <FaCss3 />
+              CSS3
+            </li>
+            <li>
+            <FaBootstrap />
+              Bootstrap
+            </li>
+            <li>
+            <SiHandlebarsdotjs />
+              Handlebars
+            </li>
+            <li>
+            <FaReact />
+              React.js
+            </li>
+            <li>
+            <GrMysql />
+              mySQL
+            </li>
+            <li>
+            <DiMongodb />
+              noSQL/MongoDB
+            </li>
           </ul>
+          <div className="pro-tech-list">
+            <h2>Professional Tech</h2>
+            <ul>
+              <li>
+              <SiMicrosoftoffice />
+                Microsoft 365
+              </li>
+              <li>
+              <FaGoogle />
+                Google Suite
+              </li>
+              <li>
+              <PiVinylRecordLight />
+                Logic Pro X
+              </li>
+              <li>
+              <FaWordpress />
+                WordPress
+              </li>
+              <li>
+              <FaComputer />
+                Mac & PC
+              </li>
+              <li>
+              <SiAdobeindesign />
+                Adobe InDesign
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="education">
-          <h3>The University of Colorado Denver</h3>
-          <p>B.S. | Music Business</p>
-        </div>
-        <div className="education">
-          <h3>The University of Texas at Austin</h3>
-          <p>Certificate | Full-Stack Web Development</p>
+          <h2>Education</h2>
+          <div className="cu-denver">
+            <h3>The University of Colorado Denver</h3>
+              <p>B.S. | Music Business</p>
+          </div>
+          <div className="uta">
+            <h3>The University of Texas at Austin</h3>
+              <p>Certificate | Full-Stack Web Development</p>
+          </div>
         </div>
       </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     </div>
   )
 }
